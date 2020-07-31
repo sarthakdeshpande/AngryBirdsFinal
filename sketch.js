56b1c9e6eaf1b439219685f4d217636a3dd6b52f
sarthakdeshpande <satish1916@gmail.com>
2020-07-31 17:45:37 +0530

    debbuging errors

--- sketch.js
+++ sketch.js
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -93,8 +93,11 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32  && gameState === "launched" && bird.body.speed < 1){
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body , {x:200,y:50 })
        slingshot.attach(bird.body);
+     gameState ="onSling"   
     }
 }
 
